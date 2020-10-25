103a5f8f7fca68a59736afee660ff121cc4b263d
WolfenCLI <WolfenCLI@zohomail.eu>
2020-10-25 15:58:20 +0100

    adds dwm to display manager

--- dwm.sh
+++ dwm.sh
@@ -5,4 +5,13 @@ git clone https://github.com/WolfenCLI/dwm.git /tmp/dwm
 cd /tmp/dwm/
 make
 make install
+cat << EOF > /usr/share/xsessions/dwm.desktop
+[Desktop Entry]
+Encoding=UTF-8
+Name=Dwm
+Comment=Dynamic Window Manager
+Exec=dwm
+Icom=dwm
+Type=XSession
+EOF
 
