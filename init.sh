1c6856ad9c56f460eaaf545779624dd9d5c79eb2
WolfenCLI <WolfenCLI@zohomail.eu>
2020-10-29 18:22:53 +0100

    removes su usage (interrupts script)

--- init.sh
+++ init.sh
@@ -115,8 +115,7 @@ EndSection
 EOF
 
 ##### DOTFILES
-su "$USERNAME"
-cd "$HOME"
+USER_HOME="/home/${USERNAME}"
 
 # ZSH
 sudo pacman -S --noconfirm zsh curl git powerline-fonts zsh-autosuggestions
@@ -124,37 +123,37 @@ read -s "After the oh-my-zsh prompt appears, press ctrl-D to continue"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # Oh-my-zsh theming
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$USER_HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-wget -O "$HOME"/.zshrc https://raw.githubusercontent.com/WolfenCLI/zsh-dotfiles/master/.zshrc
+wget -O "$USER_HOME"/.zshrc https://raw.githubusercontent.com/WolfenCLI/zsh-dotfiles/master/.zshrc
 
 # Alacritty
-mkdir "$HOME/.config/alacritty"
-wget https://raw.githubusercontent.com/WolfenCLI/alacritty-dotfiles/master/alacritty.yml -O "$HOME/.config/alacritty/alacritty.yml"
+mkdir "$USER_HOME/.config/alacritty"
+wget https://raw.githubusercontent.com/WolfenCLI/alacritty-dotfiles/master/alacritty.yml -O "$USER_HOME/.config/alacritty/alacritty.yml"
 
 # Neovim
-mkdir "$HOME/.config/nvim"
-wget https://raw.githubusercontent.com/WolfenCLI/neovim-dotfiles/master/init.vim -O "$HOME/.config/nvim/init.vim"
+mkdir "$USER_HOME/.config/nvim"
+wget https://raw.githubusercontent.com/WolfenCLI/neovim-dotfiles/master/init.vim -O "$USER_HOME/.config/nvim/init.vim"
 
 # .profile, .tmux.conf
-wget https://raw.githubusercontent.com/WolfenCLI/Personal-Wiki/master/.profile -O "$HOME/.profile"
-wget https://raw.githubusercontent.com/WolfenCLI/Personal-Wiki/master/.tmux.conf -O "$HOME/.tmux.conf"
+wget https://raw.githubusercontent.com/WolfenCLI/Personal-Wiki/master/.profile -O "$USER_HOME/.profile"
+wget https://raw.githubusercontent.com/WolfenCLI/Personal-Wiki/master/.tmux.conf -O "$USER_HOME/.tmux.conf"
 
 # My own custom scripts
-mkdir -p "$HOME/.local/scripts"
-mkdir -p "$HOME/.local/bin"
-wget https://raw.githubusercontent.com/WolfenCLI/Personal-Wiki/master/sync-pull -O "$HOME/.local/scripts/sync-pull.sh"
-wget https://raw.githubusercontent.com/WolfenCLI/Personal-Wiki/master/sync-push -O "$HOME/.local/scripts/sync-push.sh"
-chmod 700 "$HOME/.local/scripts/sync-pull.sh"
-chmod 700 "$HOME/.local/scripts/sync-push.sh"
-ln -s "$HOME/.local/scripts/sync-pull.sh" "$HOME/.local/bin/"
-ln -s "$HOME/.local/scripts/sync-push.sh" "$HOME/.local/bin/"
+mkdir -p "$USER_HOME/.local/scripts"
+mkdir -p "$USER_HOME/.local/bin"
+wget https://raw.githubusercontent.com/WolfenCLI/Personal-Wiki/master/sync-pull -O "$USER_HOME/.local/scripts/sync-pull.sh"
+wget https://raw.githubusercontent.com/WolfenCLI/Personal-Wiki/master/sync-push -O "$USER_HOME/.local/scripts/sync-push.sh"
+chmod 700 "$USER_HOME/.local/scripts/sync-pull.sh"
+chmod 700 "$USER_HOME/.local/scripts/sync-push.sh"
+ln -s "$USER_HOME/.local/scripts/sync-pull.sh" "$USER_HOME/.local/bin/"
+ln -s "$USER_HOME/.local/scripts/sync-push.sh" "$USER_HOME/.local/bin/"
 
 # Adds picom to .profile
-echo "picom -b" >> "$HOME/.profile"
+echo "picom -b" >> "$USER_HOME/.profile"
 
 # Adds feh for the background
-mkdir -p "$HOME/Pictures"
-wget "${GITHUB_BASE}/background.png" -O "$HOME/Pictures"
-echo "feh --bg-scale ~/Pictures/background.png &" >> "$HOME/.profile"
+mkdir -p "$USER_HOME/Pictures"
+wget "${GITHUB_BASE}/background.png" -O "$USER_HOME/Pictures"
+echo "feh --bg-scale ~/Pictures/background.png &" >> "$USER_HOME/.profile"
 
